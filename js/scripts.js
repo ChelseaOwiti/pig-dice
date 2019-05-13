fd9215b14879fa6451ed9409ca24d7ee392f2946
Chelsea Owiti <chelseaowiti@gmail.com>
2019-05-13 02:22:55 -0400

    condition for if 1 is rolled added

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 function Name(name){
   this.name = name
   
+function on(){
+  document.getElementById('b').disabled=true;
+  document.getElementById('b2').disabled=false;
+}
+function off(){
+  document.getElementById('b2').disabled=true;
+  document.getElementById('b').disabled=false;
+} 
 }
 // user interface logic
 $(document).ready(function(event){
@@ -11,6 +19,22 @@ $(document).ready(function(event){
     document.getElementById('name').innerHTML = playerA.name
     document.getElementById('name1').innerHTML = playerB.name
   });
+  function functionA(){
+    var A = document.getElementById('score')
+    var AB = Math.floor(Math.random()*6)+1;
+    score.innerHTML = AB;
+  }
+  $("#b").click(function(){
+    var player1 = Math.ceil(Math.random()*6);
+    if (player1 ===1){
+      document.getElementById('output').innerHTML = "You rolled 1, score is 0"
+      off()
+    }
+   else if (player1 > 1){
+     on()
+    
+    }
+  })
 
 
   
