1f58bff2d1d10d4c8f51c55a870bf20605886350
Chelsea Owiti <chelseaowiti@gmail.com>
2019-05-13 11:05:43 -0400

    functionality on player 2 roll button added

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,8 @@
 function Name(name) {
   this.name = name
-
-  // function on() {
-  //   document.getElementById('b').disabled = true;
-  //   document.getElementById('b2').disabled = false;
-  // }
-  // function off() {
-  //   document.getElementById('b2').disabled = true;
-  //   document.getElementById('b').disabled = false;
-  // }
-  // var score = 0
-  // var answer = 0
 }
 
-// user interface logic
+//buisness logic
 $(document).ready(function (event) {
   $("#showme").click(function () {
     $("#show").toggle();
@@ -29,61 +18,51 @@ value2 = 0
 
 $(document).ready(function (event) {
   $("#b").click(function () {
-    var randomNo = Math.floor(Math.random()*6)+1;
-     if (randomNo === 1){
-       alert("You rolled 1")
-       roll1 = 0;
-       value1 += roll1
-       $('#add').text(value1);
-     }
-     else{
-       $('#score').text(randomNo);
-       roll1 +=randomNo;
-       $('#add').text(roll1);
+    var randomNo = Math.floor(Math.random() * 6) + 1;
+    if (randomNo === 1) {
+      alert("You rolled 1")
+      roll1 = 0;
+      value1 += roll1
+      $('#add').text(value1);
+    }
+    else {
+      $('#score').text(randomNo);
+      roll1 += randomNo;
+      $('#add').text(roll1);
 
-     }
+    }
 
   });
-  $("#b1").click(function(){
+  $("#b1").click(function () {
     value1 += roll1;
     $("#add").text(value1)
     $("#b").hide();
-  })
+  });
+  $("#b2").click(function(){
+    var random = Math.floor(Math.random() * 6) + 1;
+    if (random === 1){
+      alert("You rolled 1")
+      roll2 = 0;
+      value2 += roll2
+      $('#add1').text(value2);
+    }
+    else{
+      $('#score1').text(random);
+      roll2 += random;
+      $('#add1').text(roll2);
+    }
+  });
+
+  $(".btn").click(function () {
+    var playerA = new Name(this.name = $("input#p1").val());
+    var playerB = new Name(this.name = $("input#p2").val());
+
+    document.getElementById('name').innerHTML = playerA.name
+    document.getElementById('name1').innerHTML = playerB.name
+  });
+
 
 });
-$(".btn").click(function () {
-  var playerA = new Name(this.name = $("input#p1").val());
-  var playerB = new Name(this.name = $("input#p2").val());
 
-  document.getElementById('name').innerHTML = playerA.name
-  document.getElementById('name1').innerHTML = playerB.name
-});
-
-
-//   $("#b").click(function () {
-//     var player1 = Math.floor(Math.random()*6)+1;
-//     if (player1 === 1) {
-//       document.getElementById('output').innerHTML = "You rolled 1, score is 0";
-//       // off();
-//     }
-//     else if (player1 > 1) {
-//       var answer = score + player1;
-//       document.getElementById('score').innerHTML = answer;
-//     }
-//   });
-//   $("#b2").click(function () {
-//     var player2 = Math.floor(Math.random()*6)+1;
-//     if (player2 === 1) {
-//       document.getElementById('outputs').innerHTML = "You rolled 1, score is 0";
-//       // on()
-//     }
-//     else if (player2 > 1) {
-//       var answer = score + player2;
-//       document.getElementById('score1').innerHTML = answer;
-//     }
-
-
-
-//   });
 
 
